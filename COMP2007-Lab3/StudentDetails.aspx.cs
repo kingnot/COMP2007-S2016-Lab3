a8273865bdda28103894bbf7c53cc656231bae04
kingnot <kingnot0524@gmail.com>
2016-06-09 22:17:22 -0400

    added page size, sorting, edit and delete for the table of Student page, update view for student details page

--- COMP2007-Lab3/StudentDetails.aspx.cs
+++ COMP2007-Lab3/StudentDetails.aspx.cs
@@ -19,6 +19,34 @@ namespace COMP2007_Lab3
             //to avoid Date Conversion issue with system date format
             RangeValidator1.MinimumValue = DateTime.Today.AddYears(-16).ToShortDateString();
             RangeValidator1.MaximumValue = DateTime.Today.ToShortDateString();
+
+            if((!IsPostBack) && (Request.QueryString.Count > 0))
+            {
+                this.GetStudent();
+            }
+        }
+
+        protected void GetStudent()
+        {
+            // populate the form with existing data from the database
+            int StudentID = Convert.ToInt32(Request.QueryString["StudentID"]);
+
+            // connect to the EF DB
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                // populate a student object instance with the StudentID from the URL Parameter
+                Student updateStudent = (from student in db.Students
+                                         where student.StudentID == StudentID
+                                         select student).FirstOrDefault();
+
+                //map the student properties to the form control
+                if (updateStudent != null)
+                {
+                    LastNameTextBox.Text = updateStudent.LastName;
+                    FirstNameTextBox.Text = updateStudent.FirstMidName;
+                    EnrollmentDateTextBox.Text = updateStudent.EnrollmentDate.ToString("MM-dd-yyyy");
+                }
+            }
         }
 
         protected void CancelButton_Click(object sender, EventArgs e)
@@ -36,15 +64,32 @@ namespace COMP2007_Lab3
                 // and save a new record
                 Student newStudent = new Student();
 
+                int StudentID = 0;
+                if(Request.QueryString.Count > 0) // our URL has a StudentID in it
+                {
+                    // get the id from url
+                    StudentID = Convert.ToInt32(Request.QueryString["StudentID"]);
+
+                    //ge the current student from EF db
+                    newStudent = (from student in db.Students
+                                  where student.StudentID == StudentID
+                                  select student).FirstOrDefault();
+                }
+
                 // add form data to the new student record
                 newStudent.LastName = LastNameTextBox.Text;
                 newStudent.FirstMidName = FirstNameTextBox.Text;
                 newStudent.EnrollmentDate = Convert.ToDateTime(EnrollmentDateTextBox.Text);
 
                 // use LINQ to ADO.Net to add / insert new student into the database
-                db.Students.Add(newStudent);
 
-                //save our changes
+                //check to see if new student is being added
+                if (StudentID == 0)
+                {
+                    db.Students.Add(newStudent);
+                }
+
+                //save our changes - run an update
                 db.SaveChanges();
 
                 //redirect back to updated student page
--- COMP2007-Lab3/Students.aspx.cs
+++ COMP2007-Lab3/Students.aspx.cs
@@ -8,6 +8,7 @@ using System.Web.UI.WebControls;
 // using statements that are required to coneect to EF DB
 using COMP2007_Lab3.Models;
 using System.Web.ModelBinding;
+using System.Linq.Dynamic;
 
 namespace COMP2007_Lab3
 {
@@ -18,6 +19,8 @@ namespace COMP2007_Lab3
             //if loading the page for the first time, populate the student grid
             if (!IsPostBack)
             {
+                Session["SortColumn"] = "StudentID";
+                Session["SortDirection"] = "ASC";
                 //get the student data
                 this.GetStudents();
             }
@@ -33,6 +36,8 @@ namespace COMP2007_Lab3
          */
         protected void GetStudents()
         {
+            string sortString = Session["SortColumn"].ToString() + " " + Session["SortDirection"].ToString();
+
             //connect to EF
             using (DefaultConnection db = new DefaultConnection())
             {
@@ -41,8 +46,90 @@ namespace COMP2007_Lab3
                                 select allStudents);
 
                 //bind the result to the GridView
-                StudentsGridView.DataSource = Students.ToList();
+                StudentsGridView.DataSource = Students.AsQueryable().OrderBy(sortString).ToList();
                 StudentsGridView.DataBind();
+            }
+        }
+
+        protected void StudentsGridView_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            //store which row was clicked
+            int selectedRow = e.RowIndex;
+
+            // get the selected StudentID using the Grid's data collection
+            int StudentID = Convert.ToInt32(StudentsGridView.DataKeys[selectedRow].Values["StudentID"]);
+
+            //use EF to find the selected student form DB and remove it
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                Student deletedStudent = (from studentRecords in db.Students
+                                          where studentRecords.StudentID == StudentID
+                                          select studentRecords).FirstOrDefault();
+
+                // perform the removal in the DB
+                db.Students.Remove(deletedStudent);
+                db.SaveChanges();
+
+                // refresh the grid
+                this.GetStudents();
+            }
+        }
+
+        protected void StudentsGridView_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            // set the new page number
+            StudentsGridView.PageIndex = e.NewPageIndex;
+
+            //refresh the grid
+            this.GetStudents();
+        }
+
+        protected void PageSizeDropDownList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //set new page size
+            StudentsGridView.PageSize = Convert.ToInt32(PageSizeDropDownList.SelectedValue);
+
+            //refresh the grid
+            this.GetStudents();
+        }
+
+        protected void StudentsGridView_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            // get the column to sort by
+            Session["SortColumn"] = e.SortExpression;
+
+            // toggle the direction
+            Session["SortDirection"] = Session["SortDirection"].ToString() == "ASC" ? "DESC" : "ASC";
+
+            //refresh the grid
+            this.GetStudents();
+        }
+
+        protected void StudentsGridView_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if(IsPostBack)
+            {
+                if(e.Row.RowType == DataControlRowType.Header)  //check to see if the click is on the header row
+                {
+                    LinkButton linkbutton = new LinkButton();
+
+                    for(int index = 0; index < StudentsGridView.Columns.Count; index++)
+                    {
+                        if (StudentsGridView.Columns[index].SortExpression == Session["SortColumn"].ToString())
+                        {
+                            if (Session["SortDirection"].ToString() == "ASC")
+                            {
+                                linkbutton.Text = "<i class='fa fa-caret-up fa-lg'></i>";
+                            }
+                            else
+                            {
+                                linkbutton.Text = "<i class='fa fa-caret-down fa-lg'></i>";
+                            }
+
+                            e.Row.Cells[index].Controls.Add(linkbutton);
+                        }
+                    }
+                }
             }
         }
     }
